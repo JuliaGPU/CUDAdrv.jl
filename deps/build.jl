fa39494bb210bdb32f37af18363faa3962e6f98d
Mus M <mus-m@outlook.com>
2017-04-09 12:22:00 -0400

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -4,10 +4,10 @@ using Compat
 
 const ext = joinpath(@__DIR__, "ext.jl")
 try
-    include(joinpath(@__DIR__, "..", "src", "util", "logging.jl"))
+    include(joinpath(dirname(@__DIR__), "src", "util", "logging.jl"))
 
     libcuda_name = is_windows() ? "nvcuda.dll" : "libcuda"
-    debug("Looking for $libcuda_name...")
+    debug("looking for $libcuda_name")
 
     libcuda = Libdl.find_library(libcuda_name)
     if libcuda == ""
@@ -16,8 +16,8 @@ try
         #       (e.g. ld.so.conf) or user preferences (LD_LIBRARY_PATH)
         libcuda = Libdl.find_library(libcuda_name, ["/opt/cuda/lib", "/usr/local/cuda/lib"])
     end
-    if libcuda == ""
-        error("Could not find CUDA library -- is the CUDA driver installed?")
+    if isempty(libcuda)
+        error("Could not find CUDA library; is the CUDA driver installed?")
     end
 
     # find the full path of the library
@@ -25,11 +25,11 @@ try
     #       but the user might have run this script with eg. LD_LIBRARY_PATH set
     #       so we save the full path in order to always be able to load the correct library
     libcuda_path = Libdl.dlpath(libcuda)
-    info("Found $libcuda at $libcuda_path")
+    info("found $libcuda at $libcuda_path")
 
     # find the library vendor
     libcuda_vendor = "NVIDIA"
-    debug("Vendor: $libcuda_vendor")
+    debug("vendor: $libcuda_vendor")
 
     # find the library version
     # NOTE: should be kept in sync with src/version.jl::version()
@@ -38,21 +38,21 @@ try
     sym = Libdl.dlsym(lib, :cuDriverGetVersion)
     status = ccall(sym, Cint, (Ptr{Cint},), version_ref)
     if status != 0
-        error("Could not get CUDA library version")
+        error("could not obtain CUDA library version")
     end
     major = version_ref[] ÷ 1000
     minor = mod(version_ref[], 100) ÷ 10
     libcuda_version = VersionNumber(major, minor)
-    debug("Version: $libcuda_version")
+    debug("version: $libcuda_version")
 
     # check if we need to rebuild
     if isfile(ext)
-        debug("Checking validity of existing ext.jl...")
+        debug("checking validity of existing ext.jl")
         @eval module Previous; include($ext); end
         if  isdefined(Previous, :libcuda_version) && Previous.libcuda_version == libcuda_version &&
             isdefined(Previous, :libcuda_path)    && Previous.libcuda_path == libcuda_path &&
             isdefined(Previous, :libcuda_vendor)  && Previous.libcuda_vendor == libcuda_vendor
-            info("CUDAdrv.jl has already been built for this CUDA library, no need to rebuild")
+            info("CUDAdrv.jl has already been built for this CUDA library, no need to rebuild.")
             return
         end
     end
